f036c352dce6de0bf84f78dedcc2dfe18cb52575
Akiinro <aktakinro@gmail.com>
2021-11-30 04:08:26 -0800

    commit

--- todolistproject/settings.py
+++ todolistproject/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 from dotenv import load_dotenv, find_dotenv
-load_dotenv(find_dotenv())
+load_dotenv()
 
 
 
